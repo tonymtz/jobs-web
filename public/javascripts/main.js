775e034b2e7164a6db92698ac3bff6bad96892dd
Jorge Chavez <echavez@nearsoft.com>
2016-10-10 11:49:32 -0600

    Add Life at Nearsoft page (#8)
    * Add Life at Nearsoft page
    
    * Changes per PR comments
    
    * Add responsive design styles

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,15 @@
 // Main js file
 (function(){
   $(document).ready(function(){
-    let a = (data)  => {
-      console.log("hi");
-    }
+    var windowHeight = $(window).height();
+    var height = windowHeight - $('.top-menu').height();
+    $('.stretch-height').height(height);
+    $('.stretch-height-with').height(windowHeight);
+
+    $(window).scroll((e) => {
+        var top = -100 + $(window).scrollTop() * 0.1;
+        $('.nice-scroll-img').css('background-position-y', top+'px');
+    });
+
   });
 })();
