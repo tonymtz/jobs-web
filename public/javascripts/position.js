55ea760eae0026dc06f7320869a17c16173e528a
echavezNS <echavez@nearsoft.com>
2017-03-01 15:19:07 -0700

    Add slick slider to this is who we arepage

--- public/javascripts/position.js
+++ public/javascripts/position.js
@@ -12,4 +12,33 @@
 
   });
 
+  $(".slider-slick").slick({
+    "slidesToShow": 4,
+    "slidesToScroll": 4,
+    "centerMode": true,
+    "arrows": false,
+    "responsive": [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+
 })();
